750ca5648d8253e0c31c46e32273ed071f4c8c15
Lucas H B Medeiros <lucashenriquebarros2016@gmail.com>
2024-08-09 23:03:59 -0300

    initial commit

---
+++ src/User.java
@@ -0,0 +1,17 @@
+public class User {
+    public static void main(String[] args) {
+        AgendaContatos agenda = new AgendaContatos();
+
+        agenda.adicionarContato("Lucas", 845321457);
+        agenda.adicionarContato("Aline", 845324257);
+        agenda.adicionarContato("Camila", 845324254);
+        agenda.adicionarContato("Bob", 235458855);
+
+        agenda.exibirContatos();
+
+        agenda.removerContato("Camila");
+
+        agenda.exibirContatos();
+
+    }
+}
